bb7a516e777d801490b6befe0e49989356e90d6c
Michael Corey <mikejcorey@gmail.com>
2014-09-09 15:52:50 -0700

    initial commit. Basic emailing.

---
+++ calvin/apps/temperature/management/commands/get_current_temp.py
@@ -0,0 +1,61 @@
+import logging
+from subprocess import call
+from datetime import datetime
+import os
+import re
+
+from django.core.management.base import BaseCommand
+from django.conf import settings
+from django.core.mail import send_mail
+
+logger = logging.getLogger('cir.custom')
+
+
+class Command(BaseCommand):
+    help = "Updates Application objects' ag_use boolean using record_id."
+
+    output_path = os.path.join(settings.BASE_DIR, 'data', 'temper.out')
+    email_list = settings.EMAIL_LIST
+
+    def refresh_temp(self):
+        #output_path = os.path.join(self.working_dir, 'temper.out')
+        d = datetime.now()
+        f = open(self.output_path, 'w')
+        f.write(datetime.strftime(d, '%Y-%m-%d %X'))
+        f.write('\n')
+        f.close()
+        call('sudo temper-poll >> %s' % (self.output_path,), shell=True)
+
+    def parse_temp(self):
+
+        f = open(self.output_path, 'ru')
+        contents = f.read()
+        logger.info(contents)
+
+        timestamp_regex = r'(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})'
+        timestamp_match = re.search(timestamp_regex, contents)
+        temp_time = datetime.strptime(timestamp_match.group(0), '%Y-%m-%d %X')
+
+        temp_regex = r'(\d{1,3}\.\d\S+F)'
+        temp_match = re.search(temp_regex, contents)
+        temp = temp_match.group(0)
+
+        message = 'The time is %s, and it\'s %s at Mike\'s desk.' % (temp_time.time(), temp,)
+        logger.info(message)
+        send_mail(
+            'Important Temperature Warning',
+            message,
+            'mcorey@cironline.org',
+            self.email_list,
+            fail_silently=False
+        )
+
+    def handle(self, *args, **options):
+        try:
+            self.refresh_temp()
+            self.parse_temp()
+
+            #call_command('generate_concern_cats', interactive=True)
+            logger.info("hello, world.")
+        except AttributeError:
+            raise
---
+++ calvin/settings.py
@@ -0,0 +1,138 @@
+"""
+Django settings for calvin project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+SITE_ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'k@obs!3%=1wsx(njudsiw(2^o@%4w@fke_q797=64&df+2c*rz'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'calvin.apps.temperature',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'calvin.urls'
+
+WSGI_APPLICATION = 'calvin.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'America/Los_Angeles'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATIC_URL = '/static/'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+            'filters': ['require_debug_false']
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs', 'django.log'),
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+        'cir.custom': {
+            'handlers': ['console', 'mail_admins', 'file'],
+            'level': 'DEBUG',
+            #'filters': ['special']
+        }
+    }
+}
+
+# Allow for local (per-user) override
+try:
+    from settings_local import *
+except ImportError:
+    pass
